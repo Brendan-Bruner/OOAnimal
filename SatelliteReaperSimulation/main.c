81c024c001fcb415e4809020566f46b402d9ef74
Brendan Bruner <bbruner@ualberta.ca>
2014-11-21 00:46:45 -0700

    Removed unused header includes in main

--- SatelliteReaperSimulation/main.c
+++ SatelliteReaperSimulation/main.c
@@ -22,50 +22,10 @@
 	of http://www.FreeRTOS.org for full details.
 */
 
-
-/* System headers. */
 #include <stdio.h>
-#include <time.h>
-#include <sys/time.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sys/stat.h>
-#include <mqueue.h>
-#include <errno.h>
-#include <unistd.h>
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include "queue.h"
-#include "croutine.h"
-#include "partest.h"
-
-/* Demo file headers. */
-#include "BlockQ.h"
-#include "PollQ.h"
-#include "death.h"
-#include "crflash.h"
-#include "flop.h"
-#include "print.h"
-#include "fileIO.h"
-#include "semtest.h"
-#include "integer.h"
-#include "dynamic.h"
-#include "mevents.h"
-#include "crhook.h"
-#include "blocktim.h"
-#include "GenQTest.h"
-#include "QPeek.h"
-#include "countsem.h"
-#include "recmutex.h"
-
-#include "AsyncIO/AsyncIO.h"
-#include "AsyncIO/AsyncIOSocket.h"
-#include "AsyncIO/PosixMessageQueueIPC.h"
-#include "AsyncIO/AsyncIOSerial.h"
 
 #define MAIN_PRIO ( tskIDLE_PRIORITY + 1)
 
