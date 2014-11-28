a16e74226c014bce110ffa70f4da88886977a227
Brendan Bruner <bbruner@ualberta.ca>
2014-11-27 23:27:14 -0700

    Committing changes to Standard file

---
+++ Standard/Class.h
@@ -0,0 +1,27 @@
+/*
+ * Class.h
+ *
+ *  Created on: 2014-11-26
+ *      Author: brendan
+ */
+
+#ifndef CLASS_H_
+#define CLASS_H_
+
+#define Constructor(D)	void new##D(D *this)
+
+#define Class(D)	typedef struct _##D D;		\
+					Constructor(D);				\
+					struct _##D					\
+					{
+						/* Super class */
+#define Data			struct					\
+						{
+							/* Class data */
+#define Methods			} data;
+							/* Function pointers */
+#define EndClass	}
+
+#define extends(S)	S super;
+
+#endif /* CLASS_H_ */
---
+++ Standard/Trait.h
@@ -0,0 +1,26 @@
+/*
+ * Trait.h
+ *
+ *  Created on: 2014-11-26
+ *      Author: brendan
+ */
+
+#ifndef TRAIT_H_
+#define TRAIT_H_
+
+typedef void * delta_t;
+
+#define Trait(T) 	typedef struct _##T T;	\
+					struct _##T				\
+					{						\
+
+						/* Methods */
+#define EndTrait		delta_t _delta;		\
+					}
+
+#define uses(t)		t t##T;
+
+#define LinkTrait(C, t) C->t##T._delta = (delta_t)(((void *) &(C->t##T)) - (void *)C)
+#define CastTrait(t)	((void *)t - (void *)t->_delta)
+
+#endif /* TRAIT_H_ */
