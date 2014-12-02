da9fb890f7aad6738db1e5c2675f0071ce694ef5
Brendan Bruner <bbruner@ualberta.ca>
2014-12-02 12:52:29 -0700

    Added license headers to more code

--- OODragon/main/Classes/FlameLord.c
+++ OODragon/main/Classes/FlameLord.c
@@ -1,3 +1,18 @@
+/* Copyright (C) 2014 Brendan Bruner
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * bbruner@ualberta.ca
+ *
+ *  Created on: 2014-11-19
+ *      Author: brendan
+ */
 /*
  * FlameLord.c
  *
