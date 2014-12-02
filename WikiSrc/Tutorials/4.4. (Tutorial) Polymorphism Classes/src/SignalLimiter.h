f5f576070064705d2a71f07ec15a21ed704a89d0
Brendan Bruner <bbruner@ualberta.ca>
2014-12-02 12:45:39 -0700

    Added license to code

--- WikiSrc/Tutorials/4.4. (Tutorial) Polymorphism Classes/src/SignalLimiter.h
+++ WikiSrc/Tutorials/4.4. (Tutorial) Polymorphism Classes/src/SignalLimiter.h
@@ -1,3 +1,17 @@
+/* Copyright (C) 2014 Brendan Bruner
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * bbruner@ualberta.ca
+ * 
+ * Created: Dec. 2, 2014
+ */
 #ifndef SIGNAL_LIMITER_H_
 #define SIGNAL_LIMITER_H_
 
