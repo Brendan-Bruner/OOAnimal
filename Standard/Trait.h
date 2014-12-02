568d1aab765670f3819cc9047ae25b48ca8f4fc5
Brendan Bruner <bbruner@ualberta.ca>
2014-12-01 19:17:40 -0700

    Added license to Class.h and Trait.h

--- Standard/Trait.h
+++ Standard/Trait.h
@@ -1,7 +1,17 @@
-/*
- * Trait.h
+/* Trait header
+ * Copyright (C) 2014 Brendan Bruner
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *  Created on: 2014-11-26
+ * bbruner@ualberta.ca
+ *
+ *  Created on: 2014-11-19
  *      Author: brendan
  */
 
