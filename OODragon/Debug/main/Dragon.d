891d1d87d6748fa110492f01fbfcc36fc005779a
Brendan Bruner <bbruner@ualberta.ca>
2014-12-02 23:51:08 -0700

    Make custom make files for OOAnimal and OODragon since the ecilpse ones were not portable

--- OODragon/Debug/main/Dragon.d
+++
@@ -1,7 +0,0 @@
-main/Dragon.d: ../main/Dragon.c \
- /home/brendan/Endeavors/OOAnimal/git/OOAnimal/OODragon/main/include/Dragon.h \
- /home/brendan/Endeavors/OOAnimal/git/OOAnimal/OODragon/Class/Class.h
-
-/home/brendan/Endeavors/OOAnimal/git/OOAnimal/OODragon/main/include/Dragon.h:
-
-/home/brendan/Endeavors/OOAnimal/git/OOAnimal/OODragon/Class/Class.h:
